c3ea7dfb60d0bee75b984060105147d2a6fa2463
Claire LEHOUCQ <c.lehoucq@gmail.com>
2014-05-13 00:24:00 +0200

    Création d'un controlleur et d'une vue pour les plans

--- src/Cubbyhole/WebSiteBundle/Controller/DefaultController.php
+++ src/Cubbyhole/WebSiteBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Cubbyhole\WebSiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -16,4 +17,14 @@ class DefaultController extends Controller
     {
         return array('name' => "ok");
     }
+
+    /**
+     * @Route("/plans")
+     * @Template()
+     */
+    public function planAction()
+    {
+       return ['plans' => $this->get("api.plan")->findAll()];
+    }
+
 }
