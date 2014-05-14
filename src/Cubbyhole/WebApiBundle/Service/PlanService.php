4fb696db039e584dc999abf96ef0735cdcd20270
Claire LEHOUCQ <c.lehoucq@gmail.com>
2014-05-14 15:31:25 +0200

    Factorisation de PlanService et injection des dépendances.

--- src/Cubbyhole/WebApiBundle/Service/PlanService.php
+++ src/Cubbyhole/WebApiBundle/Service/PlanService.php
@@ -2,17 +2,39 @@
 
 namespace Cubbyhole\WebApiBundle\Service;
 
-use Buzz\Browser;
-
 class PlanService
 {
+    private $baseUrl = 'http://localhost:3000/plans';
+    private $browser;
+    private $username;
+    private $password;
+
+    function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+    }
+    
+    public function setBrowser($browser)
+    {
+        $this->browser = $browser;
+    }
+
     public function findAll() 
     {
-        $browser = new Browser();
-        $response = $browser->get('http://localhost:3000/plans', [
-            "Authorization" => "Basic dXNlcjpwYXNz"
+        return $response = $this->get();
+    }
+    
+    public function findOne($id)
+    {
+        return $response = $this->get("/".$id);
+    }
+    
+    private function get($path = '/')
+    {
+        $response = $this->browser->get($this->baseUrl.$path, [
+            "Authorization" => "Basic ".base64_encode($this->username.":".$this->password)
         ]);
         return json_decode($response->getContent());
     }
 }
-
